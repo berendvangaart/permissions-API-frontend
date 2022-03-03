1cc6b54573ab67d038f2a5dbe9bca8a20f08ab4b
Berend <berendvangaart@gmail.com>
2022-03-03 21:04:00 +0100

    nested dashboard to different View

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+</script>
+
+<template >
+  <div class="container">
+    <div class="sidebarContainer">
+
+      <nav class="pageList">
+        <RouterLink class="link" to="/">Home</RouterLink>
+        <RouterLink class="link" to="/about">About</RouterLink>
+      </nav>
+    </div>
+
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/global.scss';
+
+.container {
+  @include flex-row;
+}
+
+.pageList {
+  @include flex-col;
+  padding: 20px;
+}
+
+.link {
+  color: white;
+  text-decoration: none;
+}
+
+.sidebarContainer {
+  background: $base-color;
+  height: 100vh;
+  width: 300px;
+}
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <h1>hello</h1>
+
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style scoped>
+
+</style>2

613c9a61fbd9b1a0744e716a48bd4e402586c549
Berend <berendvangaart@gmail.com>
2022-03-10 19:52:43 +0100

    show example counter store in home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import {useCounterStore} from "@/stores/counter";
+import {storeToRefs} from "pinia";
+
+const counterStore = useCounterStore();
+const {counter, name} = storeToRefs(counterStore)
 </script>
 
 <template>
   <main>
-    <h1>hello0000</h1>
-<!--    <TheWelcome />-->
+    <h4>{{counter}}</h4>
+    <h4>{{name}}</h4>
+
+    <button @click="counterStore.increment()">click</button>
+
   </main>
 </template>

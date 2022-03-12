34ea33921841602c0fd021109a3d963c15e80c15
Noah Jagnarain <noah.jagnarain@hva.nl>
2022-03-12 18:03:48 +0100

    start design dashboard view

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Dashboard from '../views/Dashboard.vue'
+import ApiView from '../views/ApiView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,11 @@ const router = createRouter({
           // this generates a separate chunk (About.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
           component: () => import('../views/AboutView.vue')
+        },
+        {
+          path: '/api',
+          name: 'api',
+          component: ApiView
         }
       ]
     },
---
+++ src/views/ApiView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <MDBTable >
+      <thead>
+      <tr>
+        <th>api name</th>
+        <th>afd</th>
+      </tr>
+      </thead>
+      <tbdody v-for="api in apiData">
+        <tr>
+          <td>{{api.name}} </td>
+          <td>{{api.base_path}} </td>
+        </tr>
+      </tbdody>
+    </MDBTable>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {MDBTable} from "mdb-vue-ui-kit";
+
+export default {
+  data() {
+    return {
+      apiData: null
+    }
+  },
+  async mounted() {
+    const response = await fetch("http://127.0.0.1:8000/api/apis/")
+    const data = await response.json();
+    this.apiData = data;
+  }
+}
+</script>
+
+
